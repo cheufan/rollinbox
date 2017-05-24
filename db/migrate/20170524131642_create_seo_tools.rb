53320f4f42ed1f86cb19638cf8947f21dcebf6f4
François <francois.daneau@begital.com>
2017-05-24 16:14:25 +0200

    Add seo tools (Google analytic, Google tag manager, Google site verification

--- db/migrate/20170524131642_create_seo_tools.rb
+++ db/migrate/20170524131642_create_seo_tools.rb
@@ -1,4 +1,4 @@
-class CreateSeoTools < ActiveRecord::Migration
+class CreateSeoTools < ActiveRecord::Migration[5.0]
   def change
     create_table :seo_tools do |t|
       t.string :code, index: {unique: true}, null: false
